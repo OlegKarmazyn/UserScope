bf330dfbf05c832a9797c30160133be4e091ca83
OlegKarmazyn <ol0321k@gmail.com>
2025-05-12 01:56:14 +0200

    defined user roles

---
+++ user_scope/src/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, ['label' => 'User name'])
+            ->add('password', PasswordType::class, ['label' => 'Password'])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Roles',
+                'choices' => [
+                    'View list of users' => 'ROLE_USER_LIST',
+                    'Editing Users' => 'ROLE_USER_EDIT',
+                    'View results of the algorithm' => 'ROLE_ALGORITHM_VIEW',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
+
+
+
